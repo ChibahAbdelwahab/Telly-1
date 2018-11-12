1db6d83e36138de9a5a23ba48169ed3ff4682391
Chouaki-Salim <fs_chouaki@esi.dz>
2018-11-12 09:56:33 +0100

    User Management Module, feature add account and user

--- src/com/telly/controllers/HomeController.java
+++ src/com/telly/controllers/HomeController.java
@@ -1,27 +1,31 @@
 package com.telly.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.telly.dao.FormValidationGroup;
+import java.security.Principal;
+import java.util.List;
+import com.telly.service.UserService;
+
+
+import com.telly.dao.User;
 
 
 @Controller
 public class HomeController {
-	
 	@RequestMapping("/")
 	public String showHome() {
 		
 		return "home";
 	}
-
-	@RequestMapping("/login")
-	public String showLogin() {
-		return "login";
 	}
 
-	@RequestMapping("/loggedout")
-	public String showLogout() {
-		return "loggedout";
-	}
 
 
 }
