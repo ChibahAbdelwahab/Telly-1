8feed634820ed6a153bc426a457844bf8718b39b
Chouaki-Salim <fs_chouaki@esi.dz>
2018-11-12 09:50:48 +0100

    Security Module, features login and logout

--- src/com/telly/controllers/UserController.java
+++ src/com/telly/controllers/UserController.java
@@ -6,14 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
-	
-	@RequestMapping("/")
-	public String showHome() {
-		
-		return "home";
-	}
-
-	@RequestMapping("/login")
+@RequestMapping("/login")
 	public String showLogin() {
 		return "login";
 	}
